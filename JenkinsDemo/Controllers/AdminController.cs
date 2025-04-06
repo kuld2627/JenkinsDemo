7ac8459d4478b4925e9aadf0473575fe3eab459d
Kuld@2667 <kuldeepkumar.hsihe@gmail.com>
2025-04-06 13:04:34 +0530

    new Auth controller

---
+++ JenkinsDemo/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet(Name = "AdminName")]
+        public string Get()
+        {
+            return "Kuldeep kumar";
+        }
+    }
+}
