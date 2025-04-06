8a642fb30871f09d4d34eefb39b2403dfb765136
Kuld@2667 <kuldeepkumar.hsihe@gmail.com>
2025-04-06 11:25:46 +0530

    dev test changes.

--- JenkinsDemo/WeatherForecast.cs
+++ JenkinsDemo/WeatherForecast.cs
@@ -2,6 +2,7 @@ namespace JenkinsDemo
 {
     public class WeatherForecast
     {
+        //dev test property
         public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
